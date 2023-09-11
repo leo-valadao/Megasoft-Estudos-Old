64babe1199d09a617585a82cf918a25bd1586857
Leonardo Valadão de Oliveira <leonardovaladaooliveira@hotmail.com>
2023-09-11 14:03:12 -0300

    Atualizar repositório com versao funcional

--- 04 - Estudo - Spring Framework/src/main/java/com/leonardovaladao/spring/config/SecurityConfig.java
+++ 04 - Estudo - Spring Framework/src/main/java/com/leonardovaladao/spring/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.leonardovaladao.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,34 +13,36 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        // UserDetails admin = User.withUsername("admin").password(encoder.encode("admin")).roles("ADMIN").build();
-        // UserDetails user = User.withUsername("user").password(encoder.encode("user")).roles("USER").build();
-        // return new InMemoryUserDetailsManager(admin, user);
+        UserDetails admin = User.withUsername("admin").password(encoder.encode("123")).roles("ADMIN").build();
+        UserDetails user = User.withUsername("user").password(encoder.encode("123")).roles("USER").build();
 
-        return new LoginService();
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers(HttpMethod.GET, "/anime").permitAll()
-                .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
-                .rememberMe(Customizer.withDefaults());
-
-        return http.build();
+        return new InMemoryUserDetailsManager(admin, user);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(
+                        auth -> auth.requestMatchers("/anime/*").authenticated()
+                                .anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                // .rememberMe(Customizer.withDefaults())
+                .build();
+
+    }
+
 }
